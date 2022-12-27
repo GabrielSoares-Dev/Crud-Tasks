d6b8d24d12cfc2ffb59d84639a96047f4ebc59a9
Gabriel Soares Maciel <gsm2015@outlook.com.br>
2022-12-26 21:58:58 -0300

    update image

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 
 $router->get('/', function () use ($router) {
     return response()->json([
-        'message' => 'doido',
+        'message' => 'doidoddd',
         'version' => $router->app->version(),
     ]);
 });
