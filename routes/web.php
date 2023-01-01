f62521fbf35c18732a405e8ecd9e7ce6276ae292
Gabriel Soares Maciel <gsm2015@outlook.com.br>
2023-01-01 17:53:24 -0300

    fix docker

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 
 $router->get('/', function () use ($router) {
     return response()->json([
-        'message' => 'hey',
+        'message' => 'teste1',
         'version' => $router->app->version(),
     ]);
 });
